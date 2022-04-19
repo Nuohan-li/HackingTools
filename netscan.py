e18b404fa72fe120184fddd244a71391f812ef68
Nuohan Li <linuohan.fred22@gmail.com>
2022-04-19 17:29:47 -0400

    added portscan

--- netscan.py
+++ netscan.py
@@ -1,27 +1,25 @@
 import scapy.all as scapy
 import optparse
-
+import requests
 # allow user to get the MAC address of the desired device only or a IP prefix
 parser = optparse.OptionParser()
-parser.add_option('-t', dest='target', help='Enter the IP address of the target')
+parser.add_option('-t', dest='target', help='Enter the IP address of the target or an IP prefix')
 (option, arg) = parser.parse_args()
 
-# an ARP request that says "ARP who has 192.168.2.133, says 192.168.2.145"
+# creating an ARP request that says "ARP who has 192.168.2.133, says 192.168.2.145"
 arp_request = scapy.ARP()
 # pdst field is the ARP target's IP address
+# print(arp_request.show())
 if option.target:
     arp_request.pdst = option.target
 else:
     arp_request.pdst = '192.168.2.0/24'
-# print(arp_request.summary())
 # returns a list of options that can be set
 # scapy.ls(scapy.ARP())
 
 # set the destination MAC to broadcast MAC. scapy.Ether() returns 90:21:53:eb:be:78 > ff:ff:ff:ff:ff:ff (0x9000)
 broadcast = scapy.Ether()
 broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-#print(broadcast.summary())
-# scapy.ls(scapy.Ether())
 
 # create an ARP broadcast packet
 arp_broadcast = broadcast/arp_request
@@ -30,12 +28,13 @@ arp_broadcast = broadcast/arp_request
 # send the broadcast packet and captures the responses
 answered_packets = scapy.srp(arp_broadcast, timeout=1)[0]
 # response is Ether / ARP who has 192.168.2.133 says 192.168.2.145 ==> Ether / ARP is at 00:0c:29:99:e9:2d says 192.168.2.133 / Padding
-# print(answered_packets.summary())
+print(answered_packets.show())
 # getting the ARP response and getting the target IP address and MAC address, also display the result
-
 print('------------------------------------------------------')
 print(' IP Address\t\t\tMAC Address')
 print('------------------------------------------------------')
 for element in answered_packets:
+    # print(element)
+    # vendor = requests.get('http://api.macvendors.com/' + element[1].hwsrc).text
     print(' ' + element[1].psrc + '\t\t\t' + element[1].hwsrc)
     print('------------------------------------------------------')
---
+++ portscan.py
@@ -0,0 +1,44 @@
+import optparse
+import socket
+
+# add options 
+parser = optparse.OptionParser()
+parser.add_option('-i', dest='target_ip', help="enter the target device's IP address")
+parser.add_option('-p', dest='protocol', help="enter the protocol you are scanning for, default = tcp")
+parser.add_option('-r', dest='ports', help="enter how many ports do you want to scan, default = 65535")
+(options, arg) = parser.parse_args()
+
+ports = 0
+protocol = ""
+
+# force user to enter an IP address
+if not options.target_ip:
+    print("please provide target's IP address")
+    quit()
+
+if not options.ports:
+    ports = 65535
+else:
+    ports = int(options.ports)
+
+if not options.protocol:
+    protocol = "tcp"
+else:
+    protocol = options.protocol
+
+# creating a socket and try to connect to every single port of the target to determine which port on target machine is
+# open, and what service is running on that port
+for port in range(1, ports):
+    try:
+        sock = socket.socket()
+        sock.connect((options.target_ip, port)) 
+        print("open port: " + str(port) + "   " + "service: " + socket.getservbyport(port, protocol))   
+        sock.close()
+    except socket.error:
+        print("unable to connect to this address")
+        quit()
+    except Exception:
+        pass
+
+print("ports scanned")
+
