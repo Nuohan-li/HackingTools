a2e914334cc9b3fbfda1af508ec59a2d869868b7
NUOHAN LI <linuohan.fred22@gmail.com>
2022-11-16 19:06:22 -0500

    added portscanner to terminal app

--- hack_term.py
+++ hack_term.py
@@ -3,6 +3,7 @@ import os
 import platform
 import socket
 import time
+from misc import * 
 
 path = '/'
 host_name = socket.gethostname()
@@ -15,8 +16,34 @@ def ping(host, count):
 
 print("Hack Terminal")
 while(True):
-    cmd = input(">>>")
-    if cmd == "ping":
-        host = input("enter website to ping")
-        count = input("provide number of ping packets to send")
-        print(ping(host, count))
+    cmd = input(">>> ")
+    cmd_list = cmd.split()
+    arg_list = cmd_list[1:]
+    # [0::2] -> starting from index 0, iterate through every other element
+    opts = arg_list[0::2]
+    args = arg_list[1::2]
+    if cmd_list[0] == "portscanner":
+        log_info("portscanner is selected")
+        port = ''
+        ip = ''
+        range = ''
+        for opt in opts:
+            if opt == '-p':
+                index = opts.index(opt)
+                port = args[index]
+                log_debug(port)
+            elif opt == '-i':
+                index = opts.index(opt)
+                ip = args[index]
+                log_debug(ip)
+            elif opt == '-r':
+                index = opts.index(opt)
+                range = args[index]
+                log_debug(range)
+        
+        if range == '':
+            subprocess.call(['python', 'portscanner.py', '-p', str(port), '-i', str(ip)])
+        else:
+            subprocess.call(['python', 'portscanner.py', '-r', str(range), '-i', str(ip)])
+
+
--- portscanner.py
+++ portscanner.py
@@ -7,7 +7,7 @@ arg_port = ''
 arg_host_ip = ''
 arg_range = ''
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "p:i:", ["port=", "host_ip="])
+    opts, args = getopt.getopt(sys.argv[1:], "p:i:r:", ["port=", "host_ip="])
 except:
     log_error("please provide a port number and IP address")
     sys.exit(2)
@@ -25,14 +25,27 @@ socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # host_ip = '192.168.1.29'
 # port = 80
 
-log_info("Host IP and port specified is IP: {} Port: {}".format(arg_host_ip, arg_port))
-# connect_ex: https://docs.python.org/3/library/socket.html
-def port_scan(arg_port):
-    if socket1.connect_ex((arg_host_ip,arg_port)):
-        log_info("port {} is closed".format(arg_port))
+def port_scan(ip, port):
+    socket1.settimeout(10)
+    if socket1.connect_ex((ip,int(port))):
+        log_info("port {} is closed".format(port))
     else:
-        log_info("port {} is open".format(arg_port))
+        log_info("port {} is open".format(port))
+
+if arg_range == '':
+    log_info("Host IP and port specified is IP: {} Port: {}".format(arg_host_ip, arg_port))
+else:
+    log_info("Host IP and range specified is IP: {} Range: {}".format(arg_host_ip, arg_range))
+    log_info("Scanning ports of range {}".format(arg_range))
+    starting_index = int(arg_range.split('-')[0])
+    print(starting_index)
+    end_index = int(arg_range.split('-')[1])
+    while starting_index != end_index:
+        print(starting_index)
+        port_scan(arg_host_ip, starting_index)
+        starting_index += 1 
+# connect_ex: https://docs.python.org/3/library/socket.html
 
 
+port_scan(arg_host_ip, arg_port)
 
-port_scan(arg_port)
