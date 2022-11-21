a0eb15fb2c13c69dd3a6cea73239f458f19ab607
NUOHAN LI <linuohan.fred22@gmail.com>
2022-11-20 20:49:57 -0500

    added progress bar(not working)

--- misc.py
+++ misc.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from termcolor import colored
+import math 
+import time
+import sys
 
 now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
@@ -17,3 +20,18 @@ def log_error(msg=""):
 
 def log_notice(msg=""):
     print('|' + now + '| NOTICE: ' + colored(msg, 'magenta'))
+
+def progress_bar(progress, total):
+    percent = 100 * (progress / float(total))
+    bar = '#' * int(percent) + '_' * (100 - int(percent))
+    print(f"\r|{bar}| {percent:.2f}%", end="\r", flush=True)
+    # time.sleep(1)
+
+
+numbers  = [x * 5 for x in range (2000, 3000)]
+result = []
+
+progress_bar(0, len(numbers))
+for i, x in enumerate(numbers):
+    result.append(math.factorial(x))
+    progress_bar(i + 1, len(numbers))
